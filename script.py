5e8bc49e80c6079b2a88a1a6a0c414671033a78f
Student <ohi-ii@att.net>
2021-09-16 23:42:35 -0400

    self download

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests
 
 def main():
-    r = requests.get('https://www.google.com')
+    r = requests.get('https://raw.githubusercontent.com/Ohi-Ahimie/CMPUT_404_Lab1/master/script.py')
     print(r.text)
 
 main()
